a57f49add20b70585495240eb6ee316c034f4ad2
Berry <berry.ventura@kin.org>
2019-08-07 17:31:39 +0300

    add back headers, add back thresholds, change package.json

--- scripts/src/config.ts
+++ scripts/src/config.ts
@@ -6,4 +6,7 @@ export const APP_ID_REGEX: RegExp = new RegExp("^[a-zA-Z0-9]{3,4}$");
 export const MEMO_LENGTH_ERROR: string = "Memo must be up to 28 characters, including memo prefix.";
 export const MEMO_LENGTH: number = 21;
 
-export const GLOBAL_RETRY = { retries: 4 };
+export const GLOBAL_HEADERS = new Map<string, string>()
+	.set("kin-sdk-web-version", PACKAGE_JSON.version);
+
+export const GLOBAL_RETRY =  { retries: 4 };
