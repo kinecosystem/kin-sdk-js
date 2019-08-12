11b1e489f2e5bc32392370d9c10e15587e2eb754
Aryeh Katz <in@akatz.io>
2019-08-12 16:04:43 +0300

    more enhances + bump

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,81 +1,14 @@
 import {
-	CreateKinAccountParams,
-	FriendBotParams,
-	KinClient,
-	PaymentListenerParams,
-	TransactionHistoryParams
-} from "./kinClient";
-import { CreateAccountParams, GetTransactionParams, KinAccount, SendKinParams } from "./kinAccount";
+	KinClient} from "./kinClient";
 import { Environment } from "./environment";
-import {
-	AccountData,
-	AssetType,
-	Balance,
-	CreateAccountTransaction,
-	OnPaymentListener,
-	PaymentListener,
-	PaymentTransaction,
-	RawTransaction,
-	Transaction
-} from "./blockchain/horizonModels";
 import KeystoreProvider from "./blockchain/keystoreProvider";
 import { KeyPair } from "./blockchain/keyPair";
-import { Keypair as BaseKeyPair } from "@kinecosystem/kin-sdk";
-import {
-	AccountExistsError,
-	AccountNotActivatedError,
-	BadRequestError,
-	ErrorType,
-	FriendbotError,
-	HorizonError,
-	InternalError,
-	InvalidAddressError,
-	InvalidDataError,
-	KinSdkError,
-	LowBalanceError,
-	NetworkError,
-	NetworkMismatchedError,
-	ResourceNotFoundError,
-	ServerError,
-} from "./errors";
-import { Transaction as XdrTransaction } from "@kinecosystem/kin-sdk";
+import { Keypair as BaseKeyPair , Transaction as XdrTransaction} from "@kinecosystem/kin-sdk";
 export {
 	KeystoreProvider,
 	KinClient,
-	KinAccount,
 	Environment,
-	Transaction,
 	XdrTransaction,
-	RawTransaction,
-	PaymentTransaction,
-	CreateAccountTransaction,
-	AccountData,
-	OnPaymentListener,
-	PaymentListener,
-	AssetType,
-	Balance,
 	KeyPair,
-	BaseKeyPair,
-	CreateKinAccountParams,
-	TransactionHistoryParams,
-	PaymentListenerParams,
-	FriendBotParams,
-	GetTransactionParams,
-	CreateAccountParams,
-	SendKinParams,
-	KinSdkError,
-	NetworkError,
-	ServerError,
-	FriendbotError,
-	InvalidAddressError,
-	NetworkMismatchedError,
-	InvalidDataError,
-	BadRequestError,
-	InternalError,
-	AccountExistsError,
-	LowBalanceError,
-	AccountNotActivatedError,
-	HorizonError,
-	ResourceNotFoundError,
-	ErrorType
+	BaseKeyPair
 };
