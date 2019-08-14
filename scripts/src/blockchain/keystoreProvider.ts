0c8299e1b4b62cb9a211e26319a72b71ec4b04f9
Aryeh Katz <in@akatz.io>
2019-08-14 15:05:04 +0300

    typo

--- scripts/src/blockchain/keystoreProvider.ts
+++ scripts/src/blockchain/keystoreProvider.ts
@@ -2,5 +2,5 @@ import { Address } from "../types";
 
 export default interface KeystoreProvider {
 	accounts: Promise<Address[]>;
-	sing(accountAddress: Address, xdrTransaction: string): Promise<string>;
+	sign(accountAddress: Address, xdrTransaction: string): Promise<string>;
 }
