639ed384525b145f4556954c3999ca337990f46b
Aryeh Katz <in@akatz.io>
2019-09-18 12:09:00 +0300

    add missing export

--- web/scripts/src/index.ts
+++ web/scripts/src/index.ts
@@ -1,12 +1,13 @@
 import { KinClient } from "./kinClient";
 import { KinAccount } from "./kinAccount";
 import { ListenerRegistration } from "./listenerRegistration";
-import { KeystoreProvider } from "@kinecosystem/kin-sdk-js-common";
+import { KeystoreProvider, Environment } from "@kinecosystem/kin-sdk-js-common";
 import * as BaseSdk from "@kinecosystem/kin-sdk";
 export {
 	KinClient,
 	KinAccount,
 	ListenerRegistration as EventRegistration,
 	BaseSdk,
-	KeystoreProvider
+	KeystoreProvider,
+	Environment
 };
